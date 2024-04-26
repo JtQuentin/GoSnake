7b3708684fb0667c9923b1bff457295b63210dbd
Nicolas <nicolas.ferreira@efrei.net>
2024-04-26 14:29:18 +0200

    Squashed commit of the following:
    commit 45ccd3ede97bc17f0f5af7020311e1ebba2b386f
    Author: EfreiThibault* <thibault.esnouf@efrei.net>
    Date:   Fri Apr 26 12:45:53 2024 +0200
    
        Adding random speed mode

--- main.go
+++ main.go
@@ -196,13 +196,13 @@ func NewSnake() *Snake {
 }
 
 func (s *Snake) updateDirection() {
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) && s.Direction.X == 0 {
+	if (ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft)) && s.Direction.X == 0 {
 		s.Direction = Point{X: -1, Y: 0}
-	} else if ebiten.IsKeyPressed(ebiten.KeyRight) && s.Direction.X == 0 {
+	} else if (ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight)) && s.Direction.X == 0 {
 		s.Direction = Point{X: 1, Y: 0}
-	} else if ebiten.IsKeyPressed(ebiten.KeyUp) && s.Direction.Y == 0 {
+	} else if (ebiten.IsKeyPressed(ebiten.KeyW)  || ebiten.IsKeyPressed(ebiten.KeyUp)) && s.Direction.Y == 0 {
 		s.Direction = Point{X: 0, Y: -1}
-	} else if ebiten.IsKeyPressed(ebiten.KeyDown) && s.Direction.Y == 0 {
+	} else if (ebiten.IsKeyPressed(ebiten.KeyS)  || ebiten.IsKeyPressed(ebiten.KeyDown)) && s.Direction.Y == 0 {
 		s.Direction = Point{X: 0, Y: 1}
 	}
 	s.Move()
@@ -265,9 +265,9 @@ func (r *Renderer) drawUI(score int, gameOver bool, gameWon bool, gameStarted bo
 			text.Draw(r.screen, "Press 'R' to restart", r.face, screenWidth/2-60, screenHeight/2+16, color.White)
 			scores, err := LoadScores()
 			if err == nil {
-				startY := screenHeight/2 + 32 // Start a bit below the "Press 'R' to restart" message
+				startY := screenHeight/2 + 32 
 				for i, entry := range scores {
-					if i >= 5 { // Display top 5 scores
+					if i >= 5 { 
 						break
 					}
 					scoreLine := fmt.Sprintf("%d. %s: %d", i+1, entry.Name, entry.Score)
@@ -369,9 +369,14 @@ func (gl *GameLogic) CheckCollisions(snake *Snake, food *Food) {
 			gl.gameWon = true
 			gl.speed = 10
 		} else {
-			if gl.speed > 2 {
-				gl.speed--
+			max := 10
+			min := 0
+			if gl.speed > 5 {
+				gl.speed = gl.speed - (rand.Intn(max-min+1) + min)
+			} else {
+				gl.speed = gl.speed + (rand.Intn(max-min+1) + min)
 			}
 		}
+
 	}
 }
