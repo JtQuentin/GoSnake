1242bdad6d0a45b8db661341326d7206defb04ae
JtQuentin <quentin.jolivet@pm.me>
2024-04-26 15:38:07 +0200

    Game file

--- main.go
+++ main.go
@@ -21,27 +21,6 @@ const (
 	tileSize     = 5
 )
 
-type Game struct {
-	snake        *Snake
-	food         *Food
-	renderer     *Renderer
-	logic        *GameLogic
-	startManager *GameStartManager
-}
-
-type Drawable interface {
-	Draw(screen *ebiten.Image)
-}
-
-type Updatable interface {
-	Update() error
-}
-
-type ScoreEntry struct {
-	Name  string
-	Score int
-}
-
 // SaveScore saves the current score to a file
 func SaveScore(score int) error {
 	file, err := os.OpenFile("scores.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -82,34 +61,6 @@ func LoadScores() ([]ScoreEntry, error) {
 	})
 
 	return scores, scanner.Err()
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	g.renderer.screen = screen
-	g.renderer.drawBackground()
-	g.renderer.drawSnake(g.snake.Body)
-	g.renderer.drawFood(g.food.Position)
-	g.renderer.drawUI(g.logic.score, g.logic.gameOver, g.logic.gameWon, g.startManager.IsGameStarted())
-}
-
-func (g *Game) Layout(_, _ int) (int, int) {
-	return screenWidth, screenHeight
-}
-
-func (g *Game) restart() {
-	g.snake = NewSnake()
-	g.food = NewFood()
-	g.logic = NewGameLogic()
-}
-
-func NewGame(snake *Snake, food *Food, renderer *Renderer, logic *GameLogic, startManager *GameStartManager) *Game {
-	return &Game{
-		snake:        snake,
-		food:         food,
-		renderer:     renderer,
-		logic:        logic,
-		startManager: startManager,
-	}
 }
 
 func main() {
